c08fd80fa006e5399552c1e30a782132e6b7d8d1
Jayant <jayantjnp@gmail.com>
2016-02-18 02:29:45 +0530

    added support for serial data

--- mosca-mysql-server.js
+++ mosca-mysql-server.js
@@ -1,12 +1,11 @@
 var mosca = require('mosca');
 var env = require('./settings');//importing settings file, environment variables
 /////serial config
-/*var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/pts/8", {
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/pts/10", {
   baudrate: 9600
-});*/
+})
 /////////////////
-console.log('printing env oject ',env.mqtt);
 var id, start,stop,action,currentime, item, macid, type, flag=1;
 //mqtt config
 var mqtt    = require('mqtt');
@@ -77,7 +76,7 @@ server.on('clientConnected', function(client) {
         if (err) throw err;
         else{
             var find=rows[0]['find'];
-            console.log('Inside client connected '+find);
+           // console.log('Inside client connected '+find);
             var regex = /^([0-9a-f]{2}[:-]){5}([0-9a-f]{2})$/;
             console.log('Mac id is valid? '+regex.test(post.macid));
             if(find==0){ //check device is the new one, find=0 means new device found, no previous entry in the table
@@ -417,17 +416,69 @@ function battstatus()
 });
 }
 //serial listen
-/*serialPort.on("open", function () {
+serialPort.on("open", function () {
   console.log('open');
   var count=0
   var res, dataout;
   serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
+    var date = new Date();
+    console.log("Time: "+date);
+
     dataout=String(data);
     res = dataout.split(",");//getting strings
-    console.log(res[0]);
-    console.log(res[1]);
-    console.log(res[2]);
+    console.log(res[0]);//stores device id
+    console.log(res[1]);//stored packet number or id
+    console.log(res[2]);//gets device type
+    //find if the device is new
+    var post  = {macid: res[0]};
+      connection.query('SELECT EXISTS(SELECT * FROM devices WHERE ?) as find',post, function(err, rows, fields) {
+      //console.log('Inside client connected '+val);
+      
+        if (err) throw err;
+        else{
+            var find=rows[0]['find'];
+           // console.log('Inside client connected '+find);
+           if(find==0){ //check device is the new one, find=0 means new device found, no previous entry in the table
+               var devdis='INSERT INTO devices VALUES (DEFAULT,NULL,\''+post.macid+'\',NULL,2,1, DEFAULT,NULL,\''+res[2]+'\')';
+                connection.query(devdis, function(err, rows, fields) { //insert into the table 
+                  if (err) throw err;
+                  else
+                    console.log('New Sensor Device found, adding '+post.macid+' into device table');
+                  });
+           }
+
+           else{
+              console.log('Device '+post.macid+' reconnected');
+            //  var devdis='UPDATE devices SET status=1, type=\''+res[2]+'\' where macid=\''+post.macid+'\'';
+             // connection.query(devdis, function(err, rows, fields) { //updating device status as online if it reconnects
+                /*if (err) throw err;
+                else
+                  console.log('Device '+post.macid+' is online');*/
+            
+              //});
+            }
+
+         }
+       });
+    ///
+    if(res[2]==='bthm')
+    {
+      var sensorVal='INSERT INTO feeds VALUES (DEFAULT,\''+res[0]+'\','+res[1]+',\''+res[2]+'\','+res[3]+','+res[4]+','+res[5]+','+res[6]+',DEFAULT,DEFAULT,NULL,NULL,NULL,NULL,NULL,NULL)';
+      connection.query(sensorVal, function(err, rows, fields) { //insert into the feed table 
+        if (err) throw err;
+        else
+          console.log('Feed added for '+res[0]+' on '+date);
+        });
+    }
+    else if(res[2]==='bm')
+    {
+      var sensorVal='INSERT INTO feeds VALUES (DEFAULT,\''+res[0]+'\','+res[1]+',\''+res[2]+'\',NULL, NULL, NULL,'+res[3]+',DEFAULT,DEFAULT,NULL,NULL,NULL,NULL,NULL,NULL)';
+      connection.query(sensorVal, function(err, rows, fields) { //insert into the feed table 
+        if (err) throw err;
+        else
+          console.log('Feed added for '+res[0]+' on '+date);
+        });
+    }
     console.log(res[3]);
     count++;
     console.log('data count : ' + count);
@@ -436,6 +487,6 @@ function battstatus()
     console.log('error in reading: ' + errors);
   });
 
-});*/
+});
 /////
 
