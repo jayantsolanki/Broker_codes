182a8cc074c5f8d8b6041fa21ceb17b5fc9911ca
root <Jayant>
2016-02-21 22:28:07 +0530

    safeguarding

---
+++ settings.js
@@ -0,0 +1,17 @@
+                                
+
+module.exports={
+/************Database mysql settings************/
+localhost:'localhost',
+user:'root',
+password:'kygh012xt',
+database:'IOT',
+/***********mosca settings************/
+mhost:"10.129.28.118",
+mport:1883,
+/***********mqtt address************/
+mqtt:'mqtt://10.129.28.118'
+/***************Serial Port*******************/
+};
+//exports.mqtt:mqtt;
+
