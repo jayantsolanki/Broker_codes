4c5e0128585dd2934590538e8512bf02825fc4a9
Jayant <jayantjnp@gmail.com>
2016-03-29 19:29:49 +0530

    misc

--- pub.js
+++ pub.js
@@ -1,8 +1,13 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://10.129.28.181:1880',{encoding:'utf8', clientId: 'publisher'});
+var client = mqtt.connect('mqtt://10.129.28.118',{encoding:'utf8', clientId: 'publisher'});
 client.on('connect', function(){
     //client.publish('esp/18:fe:34:9b:74:55', "2", {qos:0, retain:false});
     //client.publish('esp/18:fe:34:9f:a1:81', "1", {qos:0, retain:true});
-      client.publish('esp/18:fe:34:9b:63:7c/battery', "3344", {qos:0, retain:false});
+      //client.publish('esp/18:fe:34:9b:63:7c/battery', "3344", {qos:0, retain:false});
+      client.publish('esp/18:fe:34:9f:a4:a7', "2", {qos:0, retain:true});
+client.publish('esp/18:fe:34:e4:c4:8f', "2", {qos:0, retain:true});
+client.publish('esp/18:fe:34:9f:a1:e3', "2", {qos:0, retain:true});
+client.publish('esp/18:fe:34:fe:b1:e4', "2", {qos:0, retain:true});
+
         client.end();
 });
--- settings.js
+++ settings.js
@@ -4,13 +4,16 @@ localhost:'localhost',
 user:'root',
 password:'jayant123',
 database:'IOT',
+database2:'thingspeak_development',
+mhost2:"10.129.139.139",
 /***********mosca settings************/
 mhost:"10.129.28.181",
-mport:1880,
+mport:1883,
 /***********mqtt address************/
 mqtt:'mqtt://10.129.28.181',
 /***************Serial Port*******************/
-portNo:'/dev/pts/10'
+portNo:'/dev/pts/10',
 /******************/
+apiKey:'KE4UZ68QJIRP5P1L' //thingspeak account apikeyclear
 };
 //exports.mqtt:mqtt;
