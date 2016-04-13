1e532ba34a3ca64361fa7eb7a3523ed7d6ba603c
Jayant Solanki <jayant@e-yantra>
2016-04-13 20:11:55 +0530

    misc

--- mosca-mysql-server.js
+++ mosca-mysql-server.js
@@ -74,17 +74,18 @@ var localdb_config={
   host     : env.localhost,
   user     : env.user,
   password : env.password,
+  socketPath: '/var/run/mysqld/mysqld.sock',
   database : env.database
 }
 var thingspeak_config={ //for thingspeak
   host     : env.mhost2,
   user     : env.user,
   password : env.password,
+  socketPath :'/var/run/mysqld/mysqld.sock',
   database : env.database2//thingspeak
 }
 var connection = mysql.createConnection(localdb_config);
 var thingspeak = mysql.createConnection(thingspeak_config);
-
 connection.connect();//general
 thingspeak.connect();//thingspeak
 //configuration ended
