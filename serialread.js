19e2d4550f092870a052fe86c348ba3016b9228a
Jayant <jayantjnp@gmail.com>
2016-04-16 14:01:36 +0530

    misc

--- serialread.js
+++ serialread.js
@@ -1,11 +1,23 @@
 var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/pts/8", {
-  baudrate: 9600
+var serialPort = new SerialPort("/dev/pts/4", {
+  baudrate: 9600,
+  stopBits: 1,
+  dataBits: 6,
+  parity: 'odd'
 });
 serialPort.on("open", function () {
   console.log('open');
+  serialPort.flush();
+
   serialPort.on('data', function(data) {
     console.log('data received: ' + data);
+  });
+
+  serialPort.on('error', function(err) {
+    console.log("Some error in reading the data "+err);
+  });
+  serialPort.on('disconnect', function(disc) {
+    console.log("Port closed: "+disc);
   });
   
 });
