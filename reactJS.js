50020355f1e7c67d37e70a729a835e633dbbf0c9
Jayant <jayantjnp@gmail.com>
2016-06-02 12:43:02 +0530

    bug fixing for low battery check

--- reactJS.js
+++ reactJS.js
@@ -320,14 +320,14 @@ function lowBattery(groupId, conditionValue){ //actionId 6
                     log.info("Battery status updated for valves in deviceNotif table, set to adverse");// need to change the whole code
                   }
                 });
-                var sensorBat='UPDATE deviceNotif SET field1=\'0\' where deviceId in (SELECT device_id FROM (SELECT device_id, field2 FROM feeds WHERE device_id=\''+deviceId+'\' ORDER BY id DESC LIMIT 1) as temp WHERE field2>\''+conditionValue+'\')';
+                /*var sensorBat='UPDATE deviceNotif SET field1=\'0\' where deviceId in (SELECT device_id FROM (SELECT device_id, field2 FROM feeds WHERE device_id=\''+deviceId+'\' ORDER BY id DESC LIMIT 1) as temp WHERE field2>\''+conditionValue+'\')';
                 connection.query(query, function(err, sensorBat, fields) {
                   if (err) 
                     log.error("Error in checking feeds entry in devices table"+err);
                   else{
                     log.info("Battery status updated for valves in deviceNotif table, set to healthy");// need to change the whole code
                   }
-                });
+                });*/
               }
 
 
