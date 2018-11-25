f6f900d0b0b3d884362e39583495fb1d34d5b503
Jayant <jayantjnp@gmail.com>
2018-11-25 13:47:24 -0500

    saving settings

--- settings.js
+++ settings.js
@@ -1,17 +1,18 @@
 module.exports={
 /************Database mysql settings************/
 localhost:'localhost',
-user:'root',
-password:'bkm012xt',
+user:'iotday',
+password:'IOTmtjayfrmbuf18#',
 database:'IOT',
 database2:'thingspeak_development',
 mhost2:"localhost",
 /***********mosca settings************/
-mhost:"192.168.1.73",
+mhost:"10.142.0.2",
 //mhost:"127.0.0.1",
 mport:1883,
 /***********mqtt address************/
-mqtt:'mqtt://192.168.1.73',
+mqtt:'mqtt://10.142.0.2',
+//mqtt:'mqtt://127.0.0.1',
 /***************Serial Port*******************/
 portNo:'/dev/pts/10',
 /******************/
